8e3bc81d794fb9c945975b51f78d69de3a87facb
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-05-21 15:10:49 +0200

    [Mod] update access control allow methods

--- src/utils/handleOptions.go
+++ src/utils/handleOptions.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func HandleOptions(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, accessToken")
 
 	// Optionally, you can set additional headers such as 'Access-Control-Max-Age' for caching
