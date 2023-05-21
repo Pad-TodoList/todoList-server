94daf1ac1798930047e02bd45401104d49b9bc3b
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-05-21 15:28:16 +0200

    [Mod] fix routes for cors policy

--- src/routes/taskRoute.go
+++ src/routes/taskRoute.go
@@ -2,23 +2,21 @@ package route
 
 import (
 	"github.com/Pad-TodoList/todoList-server/src/controllers"
-	"github.com/Pad-TodoList/todoList-server/src/middleware"
 	"github.com/Pad-TodoList/todoList-server/src/migrate"
 	"github.com/Pad-TodoList/todoList-server/src/utils"
 	"github.com/gorilla/mux"
 )
 
 func TaskRoutes(router *mux.Router, dataAccess migrate.DataAccessObject) {
-	taskRouter := router.PathPrefix("/task").Subrouter()
-	taskRouter.Use(middleware.TaskMiddleware(dataAccess))
-	taskRouter.HandleFunc("/create", controllers.CreateTask(dataAccess)).Methods("POST")
-	taskRouter.HandleFunc("/update", controllers.UpdateTask(dataAccess)).Methods("PUT")
-	taskRouter.HandleFunc("/get/{id}", controllers.GetOneTask(dataAccess)).Methods("GET")
-	taskRouter.HandleFunc("/getUser/{id}", controllers.GetUserTask(dataAccess)).Methods("GET")
-	taskRouter.HandleFunc("/delete/{id}", controllers.DeleteTask(dataAccess)).Methods("DELETE")
-	taskRouter.HandleFunc("/create", utils.HandleOptions).Methods("OPTIONS")
-	taskRouter.HandleFunc("/update", utils.HandleOptions).Methods("OPTIONS")
-	taskRouter.HandleFunc("/get/{id}", utils.HandleOptions).Methods("OPTIONS")
-	taskRouter.HandleFunc("/getUser/{id}", utils.HandleOptions).Methods("OPTIONS")
-	taskRouter.HandleFunc("/delete/{id}", utils.HandleOptions).Methods("OPTIONS")
+	//taskRouter.Use(middleware.TaskMiddleware(dataAccess))
+	router.HandleFunc("/createTask", controllers.CreateTask(dataAccess)).Methods("POST")
+	router.HandleFunc("/updateTask", controllers.UpdateTask(dataAccess)).Methods("PUT")
+	router.HandleFunc("/getTask/{id}", controllers.GetOneTask(dataAccess)).Methods("GET")
+	router.HandleFunc("/getUserTasks/{id}", controllers.GetUserTask(dataAccess)).Methods("GET")
+	router.HandleFunc("/deleteTask/{id}", controllers.DeleteTask(dataAccess)).Methods("DELETE")
+	router.HandleFunc("/createTask", utils.HandleOptions).Methods("OPTIONS")
+	router.HandleFunc("/updateTask", utils.HandleOptions).Methods("OPTIONS")
+	router.HandleFunc("/getTask/{id}", utils.HandleOptions).Methods("OPTIONS")
+	router.HandleFunc("/getUserTasks/{id}", utils.HandleOptions).Methods("OPTIONS")
+	router.HandleFunc("/deleteTask/{id}", utils.HandleOptions).Methods("OPTIONS")
 }
--- src/routes/userRoute.go
+++ src/routes/userRoute.go
@@ -2,19 +2,17 @@ package route
 
 import (
 	"github.com/Pad-TodoList/todoList-server/src/controllers"
-	"github.com/Pad-TodoList/todoList-server/src/middleware"
 	"github.com/Pad-TodoList/todoList-server/src/migrate"
 	"github.com/Pad-TodoList/todoList-server/src/utils"
 	"github.com/gorilla/mux"
 )
 
 func UserRoutes(router *mux.Router, dataAccess migrate.DataAccessObject) {
-	userRouter := router.PathPrefix("/user").Subrouter()
-	userRouter.Use(middleware.UserMiddleware(dataAccess))
-	userRouter.HandleFunc("/update", controllers.UpdateUser(dataAccess)).Methods("PUT")
-	userRouter.HandleFunc("/get/{id}", controllers.GetUser(dataAccess)).Methods("GET")
-	userRouter.HandleFunc("/delete/{id}", controllers.DeleteUser(dataAccess)).Methods("DELETE")
-	userRouter.HandleFunc("/update", utils.HandleOptions).Methods("OPTIONS")
-	userRouter.HandleFunc("/get/{id}", utils.HandleOptions).Methods("OPTIONS")
-	userRouter.HandleFunc("/delete/{id}", utils.HandleOptions).Methods("OPTIONS")
+	//userRouter.Use(middleware.UserMiddleware(dataAccess))
+	router.HandleFunc("/updateUser", controllers.UpdateUser(dataAccess)).Methods("PUT")
+	router.HandleFunc("/getUser/{id}", controllers.GetUser(dataAccess)).Methods("GET")
+	router.HandleFunc("/deleteUser/{id}", controllers.DeleteUser(dataAccess)).Methods("DELETE")
+	router.HandleFunc("/updateUser", utils.HandleOptions).Methods("OPTIONS")
+	router.HandleFunc("/getUser/{id}", utils.HandleOptions).Methods("OPTIONS")
+	router.HandleFunc("/deleteUser/{id}", utils.HandleOptions).Methods("OPTIONS")
 }
