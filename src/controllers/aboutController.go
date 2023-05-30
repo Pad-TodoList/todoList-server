386734ee7cf1ea39fc37af8474e3c4def62b3bc0
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2023-05-30 08:45:52 +0200

    [Mod] update routes in /about.json

--- src/controllers/aboutController.go
+++ src/controllers/aboutController.go
@@ -12,20 +12,19 @@ import (
 func HandlerAbout(_ migrate.DataAccessObject) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		//w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.WriteHeader(http.StatusOK)
 		routes := []models.RouteInfo{
-			{Name: "about.json", Status: models.RouteStatus{Path: "/about.json", Status: "OK"}},
+			{Name: "about.json", Status: models.RouteStatus{Path: "GET /about.json", Status: "OK"}},
 			{Name: "Create account", Status: models.RouteStatus{Path: "POST /register", Status: "OK"}},
 			{Name: "Login", Status: models.RouteStatus{Path: "POST /login", Status: "OK"}},
-			{Name: "Get user data", Status: models.RouteStatus{Path: "GET /getUser/{id}", Status: "OK"}},
-			{Name: "Update user data", Status: models.RouteStatus{Path: "PUT /updateUser/{id}", Status: "OK"}},
-			{Name: "Delete user data", Status: models.RouteStatus{Path: "DELETE /deleteUser/{id}", Status: "OK"}},
-			{Name: "Create tasks", Status: models.RouteStatus{Path: "POST /createTask", Status: "OK"}},
-			{Name: "Update task", Status: models.RouteStatus{Path: "PUT /updateTask/{id}", Status: "OK"}},
-			{Name: "Get one task", Status: models.RouteStatus{Path: "GET /getTask/{id}", Status: "OK"}},
-			{Name: "Get all user tasks", Status: models.RouteStatus{Path: "GET /getUserTasks/{id}", Status: "OK"}},
-			{Name: "Delete task", Status: models.RouteStatus{Path: "DELETE /deleteTask/{id}", Status: "OK"}},
+			{Name: "Get user data", Status: models.RouteStatus{Path: "GET /user/get/{id}", Status: "OK"}},
+			{Name: "Update user data", Status: models.RouteStatus{Path: "PUT /user/update", Status: "OK"}},
+			{Name: "Delete user data", Status: models.RouteStatus{Path: "DELETE /user/delete/{id}", Status: "OK"}},
+			{Name: "Create tasks", Status: models.RouteStatus{Path: "POST /task/create", Status: "OK"}},
+			{Name: "Update task", Status: models.RouteStatus{Path: "PUT /task/update", Status: "OK"}},
+			{Name: "Get one task", Status: models.RouteStatus{Path: "GET /task/get/{id}", Status: "OK"}},
+			{Name: "Get all user tasks", Status: models.RouteStatus{Path: "GET /task/getUser/{id}", Status: "OK"}},
+			{Name: "Delete task", Status: models.RouteStatus{Path: "DELETE /task/delete/{id}", Status: "OK"}},
 		}
 		err := json.NewEncoder(w).Encode(routes)
 		if err != nil {
